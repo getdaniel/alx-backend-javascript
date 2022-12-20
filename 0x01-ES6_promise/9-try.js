73850f99f360390b5a01905e1ebce1576b51bb72
Me <getdaniel>
2022-12-20 23:20:39 +0300

    fix the eslint error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     queue.push(mathFunction());
-  } catch (error){
+  } catch (error) {
     queue.push(String(error));
   } finally {
     queue.push('Guardrail was processed');
   }
 
-  return queue;	
+  return queue;
 }
