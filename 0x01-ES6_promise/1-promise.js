dfd7aa7cbea75323a6d7820a284227f4e311a96d
Me <getdaniel>
2022-12-20 21:26:27 +0300

    some fix on output

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'success',
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
