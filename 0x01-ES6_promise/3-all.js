8ccb2fe96fb37b1d8123ef10503e1f0b6f804988
Me <getdaniel>
2022-12-20 22:13:09 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
