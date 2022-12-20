f4996c9bb0cc8321a37ad112de8c8083e2499515
Me <getdaniel>
2022-12-20 23:18:08 +0300

    throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  
+  try {
+    queue.push(mathFunction());
+  } catch (error){
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;	
+}
