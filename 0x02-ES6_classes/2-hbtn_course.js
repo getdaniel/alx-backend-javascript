6f6fe0039691d71be46a0d3a930cd216bcbd2372
Me <getdaniel>
2022-12-23 00:51:08 +0300

    refactor for correction

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,28 +5,30 @@ export default class HolbertonCourse {
     this.students = students;
   }
 
+  get name() {
+    return this._name;
+  }
+
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
-
     this._name = value;
   }
 
-  get name() {
-    return this._name;
+  get length() {
+    return this._length;
   }
 
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
-
     this._length = value;
   }
 
-  get lenght() {
-    return this._length;
+  get students() {
+    return this._students;
   }
 
   set students(value) {
@@ -36,11 +38,6 @@ export default class HolbertonCourse {
     if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-
     this._students = value;
-  }
-
-  get students() {
-    return this._students;
   }
 }
