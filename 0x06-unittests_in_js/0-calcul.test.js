6ab3199e68273ff252bfa351f0ae5d799dc1cbe6
Me <getdaniel>
2023-02-23 12:24:27 +0300

    test file fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-    it('Rounding two integer numbers',function() {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-    });
-    it('Rounding one integer and the other float', function() {
-	assert.strictEqual(calculateNumber(1, 3.7), 5);
-    });
-    it('Rounding float numbers with one roundup', function() {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    });
-    it('Rounding float numbers with both roundup', function() {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    });
+describe('calculateNumber', () => {
+  it('rounding two integer numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('rounding one integer and the other float', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('rounding float numbers with one roundup', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('rounding float numbers with both roundup', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
