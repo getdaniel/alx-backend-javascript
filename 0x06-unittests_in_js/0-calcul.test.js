1512eac998bc6a601c2c2edbc5e6ab54607fb049
Me <getdaniel>
2023-02-23 11:37:11 +0300

    split the test case

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
-    it('should return the sum of two rounded numbers',function() {
+    it('Rounding two integer numbers',function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    it('Rounding one integer and the other float', function() {
+	assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+    it('Rounding float numbers with one roundup', function() {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+    it('Rounding float numbers with both roundup', function() {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
