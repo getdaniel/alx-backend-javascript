21c6fed0d6bf37d21146658d5071b073bdce3873
Me <getdaniel>
2023-02-23 11:41:46 +0300

    downgrade mocha

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
     it('Rounding two integer numbers',function() {
