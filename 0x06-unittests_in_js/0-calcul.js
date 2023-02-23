db4762d9c10871dc9210cdd050b40c9d1f58565a
Me <getdaniel>
2023-02-23 11:49:23 +0300

    refactor the code

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,3 @@
-function calculateNumber(a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
-    return roundedA + roundedB;
-}
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
