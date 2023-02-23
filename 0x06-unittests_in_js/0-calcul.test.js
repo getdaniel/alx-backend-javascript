010ab8207bd2cce3324afa716d338b1e63b0de37
Me <getdaniel>
2023-02-23 13:08:12 +0300

    refactorizing

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('should return the sum of two rounded numbers', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
+describe('calculateNumber', function() {
+    it('Rounding two integer numbers',function() {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('Rounding one integer and the other float', function() {
+	assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('Rounding float numbers with one roundup', function() {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('Rounding float numbers with both roundup', function() {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
 });
